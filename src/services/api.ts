968fc3b5424ebc57690a56be5c396297c96a3ee2
Sebastian Peñaherrera <sebas172010@outlook.es>
2024-07-29 23:05:31 -0500

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,29 +1,28 @@
 export class Api {
-    static baseUrl = import.meta.env.VITE_API_URL2;
+  static baseUrl = import.meta.env.VITE_API_URL2;
+
+  static async get<T>(url: string): Promise<{ statusCode: number; data: T }> {
+    const response = await fetch(`${Api.baseUrl}${url}`);
+    const dataResponse: T = await response.json();
+    return {
+      statusCode: response.status,
+      data: dataResponse,
+    };
+  }
   
-    static async get(url: string): Promise<any> {
-        const response = await fetch(`${Api.baseUrl}${url}`);
-      
-        const dataResponse = await response.json();
-        return {
-            statusCode: response.status,
-            data: dataResponse,
-        };
-    }
-    
-    static async post(url: string, data?: any): Promise<any> {
-      const response = await fetch(`${Api.baseUrl}${url}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: data ? JSON.stringify(data) : null,
-      });
+  static async post<T>(url: string, data?: any): Promise<{ statusCode: number; data: T }> {
+    const response = await fetch(`${Api.baseUrl}${url}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data ? JSON.stringify(data) : null,
+    });
   
-      const dataResponse = await response.json();
-      return {
-        statusCode: response.status,
-        data: dataResponse,
-      };
-    }
+    const dataResponse: T = await response.json();
+    return {
+      statusCode: response.status,
+      data: dataResponse,
+    };
+  }
 }
